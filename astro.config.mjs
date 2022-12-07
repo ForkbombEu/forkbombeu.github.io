38c7949ddf586cb7babed1d3b68539268ab74dca
Puria Nafisi Azizi <puria@dyne.org>
2022-12-07 11:46:45 +0100

    Track internal pages layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
-import matomo from '@jop-software/astro-matomo';
 
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://forkbomb.eu",
-  integrations: [tailwind(), sitemap(), image(), react(),
-    matomo({
-      baseUrl: "https://matomo.dyne.org/",
-      siteId: 4
-    })
-  ]
+  integrations: [tailwind(), sitemap(), image(), react()]
 });
