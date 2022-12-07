7f85643b53af1d1702c251e828587fee6fa9e2a3
Puria Nafisi Azizi <puria@dyne.org>
2022-12-07 11:25:35 +0100

    Add matomo in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,16 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import react from "@astrojs/react";
+import matomo from '@jop-software/astro-matomo';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), image(), react()]
+  integrations: [tailwind(), sitemap(), image(), react(),
+    site: "https://forkbomb.eu",
+    matomo({
+      baseUrl: "https://matomo.dyne.org/",
+      siteId: 4
+    })
+  ]
 });
